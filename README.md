ffedf2dd830943b3d059632cd4a1004ec9363f41
Mac <mac@Melvins-iMac.lan>
2023-05-20 17:52:44 -0400

    Updated README, created js, css, html files and set up html skeleton as well as implented some functions and a constructor for book objects

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # -virtual_library
+Demonstrating skills with Objects by creating a virtual library.
---
+++ index.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pages, readBook) {
+    this.title = title;
+    this.author = author
+    this.pages = pages;
+    readBook = false;
+
+}
+
+function addBookToLibrary() {
+    
+}
